311bf7f71f3f181503c6ddf8dd40c4818a14546f
asdf123445 <bronislawwroblewski05@gmail.com>
2022-10-10 20:49:57 +0200

    Zmiany do złożenia: 	nowy plik:       main_program.py  Zmiany nie przygotowane do złożenia: 	zmieniono:       objects_library.py

---
+++ main_program.py
@@ -0,0 +1,79 @@
+import objects_library
+
+#welcome screen or sth
+
+print("Gene-ops CLI version -- testing")
+
+DNA_pattern = objects_library.give_DNA()
+"\n"
+RNA_pattern = objects_library.give_RNA()
+"\n"
+
+#-----------------main program ----------------------------------------
+print(" Type one if you want to acces replication options.\n Type 2 if you want to acces transcription options.\n Type 3 if you want to acces translation options\n")
+input(a)
+if(a == 1):
+    replication_options()
+elif( a == 2):
+    transription_options()
+elif( a == 3)
+    trnslation_options()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#------------------end of main program------------------------------
+
+def replication_options():
+def transription_options():
+def trnslation_options():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# For later uses
+
+#dna = objects_library.Dna(DNA_pattern)
+#transkryptor = objects_library.Transkryptor(DNA_pattern)
+#translator = objects_library.Translator(RNA_pattern)
